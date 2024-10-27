8f7976f26e65620401bfea599951dbbc445c3987
Mrityunjay Kumar <100058180+iMrityunjayKr@users.noreply.github.com>
2024-10-27 12:31:54 +0530

    Update vite.config.js

--- Backend/Frontend/vite.config.js
+++ Backend/Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3001,
     proxy: {
       "/api": {
-        target: "http://localhost:4002",
+        target: "https://chatapp-cik5.onrender.com"
         changeOrigin: true,
       },
     },
