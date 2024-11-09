aec77cf31f198a02d5e33e4279e43e2ee5e96e40
Mrityunjay Kumar <100058180+iMrityunjayKr@users.noreply.github.com>
2024-11-09 18:59:58 +0530

    Update vite.config.js

--- Backend/Frontend/vite.config.js
+++ Backend/Frontend/vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3001,
     proxy: {
       "/api": {
-        target: "https://chatapp-cik5.onrender.com"
+        target: process.env.BACKEND_URL || "http://localhost:4002",
+        changeOrigin: true,
+        ws: true, // Enable WebSocket proxying
       },
     },
   },
+  base: process.env.NODE_ENV === "production" ? "/frontend/" : "/",
 });
