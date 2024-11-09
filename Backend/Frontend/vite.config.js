2b37da12af953919f7a3608f092f740733b23e69
Mrityunjay Kumar <100058180+iMrityunjayKr@users.noreply.github.com>
2024-11-09 19:20:27 +0530

    Update vite.config.js

--- Backend/Frontend/vite.config.js
+++ Backend/Frontend/vite.config.js
@@ -1,17 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3001,
     proxy: {
       "/api": {
-        target: process.env.BACKEND_URL || "http://localhost:4002",
+        target: "http://localhost:4002",
         changeOrigin: true,
-        ws: true, // Enable WebSocket proxying
       },
     },
   },
-  base: process.env.NODE_ENV === "production" ? "/frontend/" : "/",
 });
